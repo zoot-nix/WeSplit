1ea34c8a254333ad22ab712f018e6de7ca628ccf
Owais Shaikh <theowaiszoot@gmail.com>
2022-06-26 17:57:42 +0530

    Initial Commit

---
+++ Shared/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  Shared
+//
+//  Created by Owais Shaikh on 26/06/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
